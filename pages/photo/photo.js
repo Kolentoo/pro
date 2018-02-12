8aede8d31e94415f59ea7869bf3b21ecd21ca8af
LXQ <515835543@qq.com>
2018-02-12 23:01:15 +0800

    photo.js fixed
    Signed-off-by: LXQ <515835543@qq.com>

--- pages/photo/photo.js
+++ pages/photo/photo.js
@@ -9,7 +9,62 @@ Page({
     photos1:[],
     photos2:[],
     count1:0,
-    count2:0
+    count2:0,
+    key:'',
+    tindex:'',
+    types: ['明星','美女','动漫','宠物','设计','摄影'],
+    status:[0,1,1,1,1,1]
+  },
+  changeType(e){
+    this.setData({
+      key: e.target.dataset.text,
+      tindex: e.target.dataset.index
+    });
+
+    let self = this;
+    let words = self.data.key;
+    self.data.status.map((item,index)=>{
+      if (index === self.data.tindex){
+        self.setData({
+          'status[index]':0
+        });
+      }else{
+        self.setData({
+          'status[index]':1
+        });
+      }
+    });
+    self.data.count1 = 0;
+    self.data.count2 = 0;
+    self.data.photos1 = [];
+    self.data.photos2 = [];
+    wx.request({
+      url: `https://image.baidu.com/channel/listjson?pn=1&rn=30&tag2=%E5%85%A8%E9%83%A8&ie=utf8`,
+      data: {
+        'tag1': words
+      },
+      header: {
+        'content-type': 'json' // 默认值
+      },
+      success: function (res) {
+        self.setData({
+          photos: res.data.data
+        });
+        self.data.photos.map(item => {
+          if (self.data.count1 <= self.data.count2) {
+            self.setData({
+              photos1: [...self.data.photos1, item.thumbnail_url],
+              count1: self.data.count1 + 1
+            });
+          } else {
+            self.setData({
+              photos2: [...self.data.photos2, item.thumbnail_url],
+              count2: self.data.count2 + 1
+            });
+          }
+        });
+      }
+    });
   },
 
   /**
@@ -18,9 +73,9 @@ Page({
   onLoad: function (options) {
     let self = this;
     wx.request({
-      url: `https://www.apiopen.top/meituApi`,
+      url: `https://image.baidu.com/channel/listjson?pn=0&rn=30&tag2=%E5%85%A8%E9%83%A8&ie=utf8`,
       data: {
-        'page':1
+        'tag1':'明星'
       },
       header: {
         'content-type': 'json' // 默认值
@@ -32,12 +87,12 @@ Page({
         self.data.photos.map(item =>{ 
           if (self.data.count1 <= self.data.count2){
             self.setData({
-              photos1: [...self.data.photos1,item.url],
+              photos1: [...self.data.photos1,item.thumbnail_url],
               count1:self.data.count1+1
             });
           }else{
             self.setData({
-              photos2: [...self.data.photos2, item.url],
+              photos2: [...self.data.photos2, item.thumbnail_url],
               count2: self.data.count2 + 1
             });
           }
